384350669df2e5d27b0d265302ac91eff6500312
LTSGOD <lts0424@naver.com>
2024-11-22 03:56:49 +0000

    fix: 실습4/SwitchPolling debouncing 문제

--- 실습4/ProgrammingSwitchwithPolling/switchPolling.c
+++ 실습4/ProgrammingSwitchwithPolling/switchPolling.c
@@ -9,28 +9,26 @@
 
 int main(void) 
 {
-    int switchState;
+    int prevSwitchState = LOW;  // 이전 스위치 상태
     int ledState = LOW;
 
     wiringPiSetupGpio();
 
     pinMode(GPIO_LED, OUTPUT);
     pinMode(GPIO_SWITCH, INPUT);
-    
-    
+
     while(1) 
     {
-    
-        // Insert your code here
-        if (digitalRead(GPIO_SWITCH)) {
-            ledState = !ledState;
-
-            delay(300);
-        }
-
-        // printf("%d", ledState);
-            
-        digitalWrite(GPIO_LED, ledState);
+        int currentSwitchState = digitalRead(GPIO_SWITCH);  // 현재 스위치 상태
         
+        if (currentSwitchState == HIGH && prevSwitchState == LOW) {
+            // 버튼이 눌린 경우 (LOW -> HIGH 변화 감지)
+            ledState = !ledState;  // LED 상태 토글
+        }
+        
+        prevSwitchState = currentSwitchState;  // 현재 상태를 이전 상태로 저장
+        printf("current: %d, prev: %d\n", currentSwitchState, prevSwitchState);
+        
+        digitalWrite(GPIO_LED, ledState);
     }
 }
